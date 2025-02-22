a7c99c38820543676d1cbbe4a9f740c44523aba2
Marcus Crane <marcus@utf9k.net>
2025-02-22 20:54:41 +1300

    Fix bad naming of tables
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- scripts/sqlite_to_csv.sh
+++ scripts/sqlite_to_csv.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-TABLES=$(sqlite-utils tables nz_government.db --csv --no-headers)
+TABLES=$(sqlite-utils tables nz_government.db --csv --no-headers | tr -d '\r')
 EXPORT_FOLDER="tables_csv"
 
 mkdir -p "$EXPORT_FOLDER"
 
 for table in $TABLES; do
-    clean_table=$(echo "$table" | tr -d '?')
-    echo "Exporting $clean_table to ${EXPORT_FOLDER}/${table}.csv"
-    sqlite-utils query nz_government.db "SELECT * FROM $clean_table" --csv > "${EXPORT_FOLDER}/${clean_table}.csv"
+    echo "Exporting $table to ${EXPORT_FOLDER}/${table}.csv"
+    sqlite-utils query nz_government.db "SELECT * FROM $table" --csv > "${EXPORT_FOLDER}/${table}.csv"
 done
